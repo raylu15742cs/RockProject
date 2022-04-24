e2e7c0ac29b0904af335229311f9c46c84b354b2
raylu15742cs <raylu15742cs@gmail.com>
2022-04-24 11:49:03 -0700

    add subproblems

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("hello")
+/* Problem 1 Function Computer Play
+    Random between rock, paper, and scissor
+    Store the choice
+*/
+/* Problem 2 Player Selection
+    Allow the player to type in one of the three
+    Make sure it is case insensitive
+*/
+/* Problem 3 Rock Paper Scissor
+    Function with two parameters that takes in player and computer selection
+    Should solve which one wins 
+    Return the result of which one wins
+*/
+/* Problem 4 Counter
+    Make it a best of 5 competition
+    Make sure the result of each match is tracked
+    Stop the program after one reach 5
+*/
