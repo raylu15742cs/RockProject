cd7d422f274403bd168ae1412b8d5e1b028c91f4
raylu15742cs <raylu15742cs@gmail.com>
2022-04-24 16:36:45 -0700

    add Playround and counter

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
     Random between rock, paper, and scissor
     Store the choice
 */
+robot = 0;
+human = 0;
+score = [0,0]
 let computerSelection = function() {
     choice = Math.floor(Math.random() * 3);
     if(choice === 0) {
@@ -18,15 +21,71 @@ let computerSelection = function() {
 */
 let playerSelection = function() {
     selection = prompt("Rock, Paper, Scissor!").toLowerCase();
-    return selection;
+    let computerPLay = computerSelection();
+    playRound(selection,computerPLay);
 }
 /* Problem 3 Rock Paper Scissor
     Function with two parameters that takes in player and computer selection
     Should solve which one wins 
     Return the result of which one wins
 */
+let playRound =function(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "rock") {
+                console.log(`Result : tie ${score} You picked ${playerSelection}. Robot picked ${computerSelection}`);
+            } else if (computerSelection === "scissor") {
+                human += 1;
+                score= [human, robot];
+                console.log(`Result : tie ${score} You picked ${playerSelection}. Robot picked ${computerSelection}`);
+            } else {
+                robot += 1;
+                score= [human, robot];
+                console.log(`Result : tie ${score} You picked ${playerSelection}. Robot picked ${computerSelection}`);
+            }
+            break
+        case "paper":
+            if (computerSelection === "paper") {
+                console.log(`Result : tie ${score} You picked ${playerSelection}. Robot picked ${computerSelection}`);
+            } else if (computerSelection === "rock") {
+                human += 1;
+                score= [human, robot];
+                console.log(`Result : tie ${score} You picked ${playerSelection}. Robot picked ${computerSelection}`);
+            } else {
+                robot += 1;
+                score= [human, robot];
+                console.log(`Result : tie ${score} You picked ${playerSelection}. Robot picked ${computerSelection}`);
+            }
+            break
+        case "scissor":
+            if (computerSelection === "scissor") {
+                console.log(`Result : tie ${score} You picked ${playerSelection}. Robot picked ${computerSelection}`);
+            } else if (computerSelection === "paper") {
+                human += 1;
+                score= [human, robot];
+                console.log(`Result : tie ${score} You picked ${playerSelection}. Robot picked ${computerSelection}`);
+            } else {
+                robot += 1;
+                score= [human, robot];
+                console.log(`Result : tie ${score} You picked ${playerSelection}. Robot picked ${computerSelection}`);
+            }
+
+            break
+  }
+}
 /* Problem 4 Counter
     Make it a best of 5 competition
     Make sure the result of each match is tracked
     Stop the program after one reach 5
 */
+while (human != 5) {
+    if (robot != 5){
+        playerSelection();
+    }
+}
+if (human == 5) {
+    console.log (`You win ${score}`)
+}
+if (robot == 5) {
+    console.log (`You lose ${score}`)
+}
